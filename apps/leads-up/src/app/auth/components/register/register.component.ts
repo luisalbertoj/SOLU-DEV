2618f92531a2b158900e50b238e1fe9961696f83
luisalbertoj <luisalbertoj.tober@gmail.com>
2022-07-12 17:32:05 -0500

    dox(auth) WP

--- apps/leads-up/src/app/auth/components/register/register.component.ts
+++ apps/leads-up/src/app/auth/components/register/register.component.ts
@@ -8,18 +8,25 @@ import { RegisterService } from '@app/auth/services/register.service';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent  {
+
+  // Modelo de datos que se bindea con el html por medio del ngModel
   public registerData: RegisterModel = {
     name: '',
     password: '',
     email: '',
   }
+  // se inyecta el servicio de registro por medio del constructor
   constructor(private _registerService: RegisterService) {}
 
+  // metodo de registro envia los datos al servicio
   newUser(): void {
+    // se llama al metodo create del servicio y se pasan los datos a del modelo del formulario
     this._registerService.create(this.registerData).subscribe({
+      // manejo de la respuesta correcta del servidor
       next: (response: RegisterResModel) => {
         return console.log({response});
       },
+      // manejo de la respuesta erronea
       error: error => console.log({error})
     });
   }
